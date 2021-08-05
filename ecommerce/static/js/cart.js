6b81c90c3fa1bd0b35e8c40e12c7cca49748498a
stens0n <60202239+stens0n@users.noreply.github.com>
2021-08-05 14:44:48 -0400

    updates

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,38 +1,117 @@
 var updateBtns = document.getElementsByClassName('update-cart')
+let loading = false
+
 
 for (i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function(){
+	updateBtns[i]?.addEventListener('click', async function(){
 		var productId = this.dataset.product
 		var action = this.dataset.action
+		var price = Number(this.dataset.price)
+		var redirect = this.dataset.redirect
+
 		console.log('productId:', productId, 'Action:', action)
-
 		console.log('USER:', user)
-		if (user == 'AnonymousUser'){
-			console.log('User is not authenticated')
 
-		}else{
-			console.log('User is authenticated, sending data...')
-		}
 
+		if (loading === true) return;
+			loading = true
+			try {
+				if (user == 'AnonymousUser'){
+						addCookieItem(price, productId, action)
+				}else{
+					let data = await updateUserOrder(productId, action)
+          updateDisplay(data['cartTotal'].toFixed(2),data['cartItems'],productId,action)
+				}
+			} catch (error) {
+				console.log(error)
+			}
 	})
 }
 
+function updateDisplay(total,items,id,action){
+	// var cart = JSON.parse(getCookie('cart'))
+  var quantity_display = document.getElementById(`quantity_display${id}`)
+  var total_amount = document.getElementsByClassName("total_amount")
+  var total_items = document.getElementsByClassName("total_items")
+  try {
+    total_items[0].innerHTML = items
+    total_amount[0].innerHTML = total
+    if (action == 'add') {
+      quantity_display.innerHTML = Number(quantity_display.innerHTML) + 1;
+    }else{
+      if ((quantity_display.innerHTML - 1) <= 0) {
+        return quantity_display.parentNode.parentNode.remove()
+      }
+      quantity_display.innerHTML = Number(quantity_display.innerHTML) - 1;
+    }
+    // console.log(cart['order']['get_cart_items'])
+  } catch (error) {
+    console.info("no display found for quantity")
+  }
+}
+
+function addCookieItem(price,productId, action){
+  console.log('User is not authenticated')
+  if (action == 'add'){
+    if (cart[productId] == undefined){
+      cart[productId] = {'quantity':1}
+    }else{
+      cart[productId]['quantity'] += 1
+    }
+    cart['cartItems'] += 1
+    cart['cartTotal'] += price
+  }
+
+  if (action == 'remove'){
+    cart[productId]['quantity'] -= 1
+    if (cart[productId]['quantity'] <= 0){
+    	console.log('Item should be deleted')
+      delete cart[productId];
+    }
+    cart['cartItems'] -= 1
+    cart['cartTotal'] -= price
+  }
+
+  try{
+    let total = cart['cartTotal']
+    cart['cartTotal']=Number(total.toFixed(2))
+  }
+  catch(e){
+      console.log(e)
+    }
+
+  console.log('CART:', cart)
+  document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+
+  updateCartQuantity(cart['cartItems'])
+  updateDisplay(cart['cartTotal'],cart['cartItems'],productId,action)
+  loading = false
+  // location.reload()
+}
+
+
+
 function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
-
-		var url = '/update_item/'
-
-		fetch(url, {
-			method:'POST',
-			headers:{
-				'Content-Type':'application/json',
-			},
-			body:JSON.stringify({'productId':productId, 'action':action})
-		})
-		.then((response) => {
-		   return response.json();
-		})
-		.then((data) => {
-		    console.log('Data:', data)
-		});
+  console.log('User is authenticated, sending data...')
+    var url = '/update_item/'
+    return new Promise(resolve => {
+      fetch(url, {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+          'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'productId':productId, 'action':action})
+      })
+      .then((response) => {
+         return response.json();
+      })
+      .then((data) => {
+          // console.log(data)
+          updateCartQuantity(data['cartItems'])
+          loading = false
+          resolve(data)
+          // location.reload()
+      });
+    });
 }
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,43 +1,54 @@
 from django.shortcuts import render
-from .models import *
 from django.http import JsonResponse
 import json
-# Create your views here.
 
+from .models import *
 
 def store(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'store/store.html', context)
 
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+		cartItems = order['get_cart_items']
+
+	products = Product.objects.all()
+	context = {'products':products, 'cartItems':cartItems}
+	return render(request, 'store/store.html', context)
 
 def cart(request):
 
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+		cartItems = order['get_cart_items']
 
-    context = {'items': items, 'order': order}
-    return render(request, 'store/cart.html', context)
-
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/cart.html', context)
 
 def checkout(request):
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+		cartItems = order['get_cart_items']
 
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
-        items = []
-
-    context = {'items': items, 'order': order}
-    return render(request, 'store/checkout.html', context)
-
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
 	data = json.loads(request.body)
