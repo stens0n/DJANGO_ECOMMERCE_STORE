2ae6d8081dc3c74735d8728879788993432d722b
stens0n <60202239+stens0n@users.noreply.github.com>
2021-08-06 22:08:44 -0400

    updates

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 
 from .models import *
 
@@ -29,11 +30,12 @@ def cart(request):
 		cartItems = order.get_cart_items
 	else:
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
 
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
+
 
 def checkout(request):
 	if request.user.is_authenticated:
@@ -74,3 +76,33 @@ def updateItem(request):
 		orderItem.delete()
 
 	return JsonResponse('Item was added', safe=False)
+
+#from django.views.decorators.csrf import csrf_exempt
+
+#@csrf_exempt
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+
+		if order.shipping == True:
+			ShippingAddress.objects.create(
+				customer=customer,
+				order=order,
+				address=data['shipping']['address'],
+				city=data['shipping']['city'],
+				state=data['shipping']['state'],
+				zipcode=data['shipping']['zipcode'],
+			)
+	else:
+		print('User is not logged in')
+	return JsonResponse('Payment submitted..', safe=False)
